9ecabc7ee9a8990721db7a72db0fe86c9507e1fe
SyedTaha13 <taha@eforte.net>
2020-04-23 20:03:21 +0500

    Congragulation UI is done

---
+++ src/screen/Congratgulation.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {View , Text , StyleSheet , TextInput, TouchableOpacity , Image} from 'react-native'
+import Button from '../component/Button'
+
+
+const SignUp = () =>{
+
+    const handleButtonPressed = () =>{
+        console.log('Button is pressed')
+    }
+    const handleSkip = () =>{
+        console.log('Skip is Pressed')
+    }
+
+    return(
+        <View style={styles.container}>
+            <View style={{marginTop:120,justifyContent:'center',alignItems:'center'}}>
+                <View style={{width:150 , height:150 , backgroundColor:'#fff'}}>
+                    <Image source={require('../../assets/congrats-check.png')} style={styles.image} />
+                </View>
+                <View style={{marginTop:40}}>
+                    <Text style={{fontSize:25 , fontWeight:'bold'}}>Congratulations!</Text>
+                </View>
+            </View>
+            <View style={{width:'100%',marginTop:80}}>
+                <View style={{marginTop:20}}>
+                    <Button title='Set Up Profile' btnColor='#19479c' textSize={14}/>
+                </View>
+            </View>
+            <View style={{marginTop:20,flexDirection:'row',justifyContent:'center'}}>  
+                    <TouchableOpacity onPress={()=>handleSkip()}>
+                        <Text style={{fontSize:15}}>Skip</Text>
+                    </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+
+export default SignUp;
+
+const styles = StyleSheet.create(
+    {
+        container:{
+            flex:1,
+            backgroundColor:'#fff',
+            marginHorizontal:20
+        },
+        inputContainer:{
+            justifyContent:'center',
+            alignItems:'center',
+            marginTop:20
+        },
+        image:{
+            resizeMode:'cover',
+            height:'100%',
+            width:'100%'
+        }
+    }
+)
