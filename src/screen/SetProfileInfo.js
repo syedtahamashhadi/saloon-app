658fb186dacca436ab3b2efe3805e2cd69075d6f
SyedTaha13 <taha@eforte.net>
2020-04-25 02:16:44 +0500

    SetUserProfile UI Done

--- src/screen/SetProfileInfo.js
+++ src/screen/SetProfileInfo.js
@@ -139,7 +139,6 @@ const styles = StyleSheet.create(
     {
         container:{
             flex:1,
-            // marginHorizontal:20,
             backgroundColor:'#fff',
         },
         avatarContainer:{
